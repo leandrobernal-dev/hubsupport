ddcccc15612c4502e75ffee600eb9e1682d3e66c
leandrobernal-dev <108369489+leandrobernal-dev@users.noreply.github.com>
2023-12-13 05:09:16 +0800

    Add UserDataContextProvider to DashboardLayout

--- src/app/dashboard/layout.js
+++ src/app/dashboard/layout.js
@@ -1,12 +1,17 @@
+"use client";
+
+import UserDataContextProvider from "@/context/UserDataContext";
 import Nav from "@/layout/Nav";
 import SideNav from "@/layout/SideNav";
 
 export default function DashBoardLayout({ children }) {
   return (
-    <div className="ml-72 pt-14">
-      <Nav />
-      <SideNav />
-      {children}
-    </div>
+    <UserDataContextProvider value={{}}>
+      <div className="ml-72 pt-14">
+        <Nav />
+        <SideNav />
+        {children}
+      </div>
+    </UserDataContextProvider>
   );
 }
