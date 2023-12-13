4205ca32a64bd37ebea56332ebd076b069e39b18
leandrobernal-dev <108369489+leandrobernal-dev@users.noreply.github.com>
2023-12-14 06:59:42 +0800

    Refactor SideNav component to dynamically render navigation items based on user role

--- src/layout/SideNav.js
+++ src/layout/SideNav.js
@@ -12,8 +12,33 @@ import {
 } from "lucide-react";
 import Link from "next/link";
 
-export default function SideNav() {
-  const navItems = [
+export default function SideNav({ user }) {
+  const userNavItems = [
+    {
+      name: "Dashboard",
+      icon: <LayoutDashboard />,
+      link: "/dashboard",
+    },
+    { name: "Live Task", icon: <BadgeAlert />, link: "/dashboard/live-task" },
+
+    {
+      name: "Team Messages",
+      icon: <MessagesSquare />,
+      link: "/dashboard/messages",
+    },
+    {
+      name: "Assigned Tasks",
+      icon: <LayoutList />,
+      link: "/dashboard/assigned-tasks",
+    },
+    {
+      name: "Account Settings",
+      icon: <Settings />,
+      link: "/dashboard/settings",
+    },
+  ];
+
+  const adminNavItems = [
     {
       name: "Dashboard",
       icon: <LayoutDashboard />,
@@ -24,16 +49,10 @@ export default function SideNav() {
       icon: <Component />,
       link: "/dashboard/teams",
     },
-    { name: "Live Task", icon: <BadgeAlert />, link: "/dashboard/live-task" },
     {
       name: "Live Issues",
       icon: <BadgeAlert />,
       link: "/dashboard/live-chat",
-    },
-    {
-      name: "Assigned Tasks",
-      icon: <LayoutList />,
-      link: "/dashboard/assigned-tasks",
     },
     {
       name: "Team Messages",
@@ -41,14 +60,14 @@ export default function SideNav() {
       link: "/dashboard/messages",
     },
     {
-      name: "Account Settings",
+      name: "Admin Settings",
       icon: <Settings />,
-      link: "/dashboard/settings",
+      link: "/dashboard/admin",
     },
   ];
 
   return (
-    <aside className="border-border fixed bottom-0 left-0 top-0 w-72 border-r p-4 shadow">
+    <aside className="fixed bottom-0 left-0 top-0 w-72 border-r border-border p-4 shadow">
       <section className="flex items-center gap-4">
         <Button variant="outline" size="icon">
           <Menu className="h-4 w-4" />
@@ -62,20 +81,41 @@ export default function SideNav() {
       </section>
 
       <section className="flex w-full flex-col gap-1 pt-8">
-        {navItems.map((item, index) => {
-          return (
-            <Button
-              asChild
-              key={item.link}
-              variant="ghost"
-              className="flex w-full justify-start gap-2"
-            >
-              <Link href={item.link}>
-                {item.icon} {item.name}
-              </Link>
-            </Button>
-          );
-        })}
+        {user.role === "admin" ? (
+          <>
+            {adminNavItems.map((item, index) => {
+              return (
+                <Button
+                  asChild
+                  key={item.link}
+                  variant="ghost"
+                  className="flex w-full justify-start gap-2"
+                >
+                  <Link href={item.link}>
+                    {item.icon} {item.name}
+                  </Link>
+                </Button>
+              );
+            })}
+          </>
+        ) : (
+          <>
+            {userNavItems.map((item, index) => {
+              return (
+                <Button
+                  asChild
+                  key={item.link}
+                  variant="ghost"
+                  className="flex w-full justify-start gap-2"
+                >
+                  <Link href={item.link}>
+                    {item.icon} {item.name}
+                  </Link>
+                </Button>
+              );
+            })}
+          </>
+        )}
       </section>
     </aside>
   );
