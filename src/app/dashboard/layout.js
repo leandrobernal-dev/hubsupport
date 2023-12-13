7624042e23c684b1358eab888fdbb8e7a4b4590c
leandrobernal-dev <108369489+leandrobernal-dev@users.noreply.github.com>
2023-12-13 17:23:57 +0800

    Add user role check in DashboardLayout

--- src/app/dashboard/layout.js
+++ src/app/dashboard/layout.js
@@ -1,13 +1,23 @@
 import UserDataContextProvider from "@/context/UserDataContext";
 import Nav from "@/layout/Nav";
 import SideNav from "@/layout/SideNav";
+import User from "@/models/User";
 
-export default function DashBoardLayout({ children }) {
+import { getServerSession } from "next-auth";
+import { notFound } from "next/navigation";
+
+export default async function DashBoardLayout({ children }) {
+  const session = await getServerSession();
+  const user = await User.findOne({ email: session.user.email });
+
+  if (!user) notFound();
+
   return (
     <UserDataContextProvider>
       <div className="ml-72 pt-14">
-        <Nav />
-        <SideNav />
+        {user.role === "admin" ? "AdminDashboard" : "UserDashboard"}
+        <Nav user={user} />
+        <SideNav user={user} />
         {children}
       </div>
     </UserDataContextProvider>
