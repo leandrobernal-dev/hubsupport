30dfa981a2fe66fa4750384359cf06d3645d2e0b
leandrobernal-dev <108369489+leandrobernal-dev@users.noreply.github.com>
2023-12-14 23:08:45 +0800

    Update middleware.js to fix matcher paths

--- src/middleware.js
+++ src/middleware.js
@@ -2,5 +2,5 @@ export { default } from "next-auth/middleware";
 
 export const config = {
   //   matcher: ["/api:path*", "/((?!auth|public|favicon.ico).*)"],
-  matcher: ["/api:path*", "/dashboard:path*"],
+  matcher: ["/api/:path*", "/dashboard/:path*"],
 };
