debd94ea4ad0d2728ad99e1556785b0df09eb9d0
leandrobernal-dev <108369489+leandrobernal-dev@users.noreply.github.com>
2023-12-18 01:45:48 +0800

    Add AdminLayout component

---
+++ src/app/dashboard/admin/layout.js
@@ -0,0 +1,51 @@
+import { Separator } from "@/components/ui/separator";
+import { AdminSidebarNav } from "@/app/dashboard/admin/components/AdminSideBarNav";
+
+const sidebarNavItems = [
+  {
+    title: "Profile",
+    href: "/dashboard/admin",
+  },
+  {
+    title: "Subscription",
+    href: "/dashboard/admin/subscription",
+  },
+  {
+    title: "Account",
+    href: "/dashboard/admin/forms/account",
+  },
+  {
+    title: "Appearance",
+    href: "/dashboard/admin/forms/appearance",
+  },
+  {
+    title: "Notifications",
+    href: "/dashboard/admin/forms/notifications",
+  },
+  {
+    title: "Display",
+    href: "/dashboard/admin/forms/display",
+  },
+];
+
+export default function AdminLayout({ children }) {
+  return (
+    <>
+      <div className="hidden space-y-6 p-10 pb-16 md:block">
+        <div className="space-y-0.5">
+          <h2 className="text-2xl font-bold tracking-tight">Settings</h2>
+          <p className="text-muted-foreground">
+            Manage your account settings and set e-mail preferences.
+          </p>
+        </div>
+        <Separator className="my-6" />
+        <div className="flex flex-col space-y-8 lg:flex-row lg:space-x-12 lg:space-y-0">
+          <aside className="-mx-4 lg:w-1/5">
+            <AdminSidebarNav items={sidebarNavItems} />
+          </aside>
+          <div className="flex-1 lg:max-w-2xl">{children}</div>
+        </div>
+      </div>
+    </>
+  );
+}
