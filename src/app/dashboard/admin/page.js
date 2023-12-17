644530708ed8e4854febc67e84f1894859fd1533
leandrobernal-dev <108369489+leandrobernal-dev@users.noreply.github.com>
2023-12-18 01:46:25 +0800

    Add AdminPage component

---
+++ src/app/dashboard/admin/page.js
@@ -0,0 +1,10 @@
+import AdminContentPage from "@/app/dashboard/admin/components/AdminContentPage";
+
+export default function AdminPage() {
+  return (
+    <AdminContentPage
+      title={"Profile"}
+      description={"This is how others will see you on the site."}
+    ></AdminContentPage>
+  );
+}
