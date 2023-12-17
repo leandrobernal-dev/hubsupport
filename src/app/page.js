c80d35272f9a5762a45b6ffccf7653b87560d0d5
leandrobernal-dev <108369489+leandrobernal-dev@users.noreply.github.com>
2023-12-18 01:40:52 +0800

    Add PlansCard component and subscriptionConfig to Home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,23 @@
+import { PlansCard } from "@/components/PlansCard";
+import { subscriptionConfig } from "@/config/subscriptionConfig";
+
 export default function Home() {
-  return <main className=""></main>;
+  return (
+    <main className="pt-14">
+      Home
+      <section className="flex w-full" id="plans&pricing">
+        {subscriptionConfig.subscriptions.map((subscription, index) => (
+          <PlansCard
+            key={index}
+            className="mr-4"
+            details={{
+              title: subscription.title,
+              shortDescription: subscription.shortDescription,
+              price: subscription.price,
+            }}
+          />
+        ))}
+      </section>
+    </main>
+  );
 }
