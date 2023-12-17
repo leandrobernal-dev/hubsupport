be8942396cf0da14ed8d29ed0f9f7950a5509a8a
leandrobernal-dev <108369489+leandrobernal-dev@users.noreply.github.com>
2023-12-18 01:51:31 +0800

    Add Checkout component for product checkout

---
+++ src/app/product/checkout/components/Checkout.js
@@ -0,0 +1,78 @@
+"use client";
+
+import { Check } from "lucide-react";
+import OrderSummary from "@/app/product/checkout/components/OrderSummary";
+import Link from "next/link";
+import CheckOutForm from "@/app/product/checkout/components/CheckOutForm";
+import { Separator } from "@/components/ui/separator";
+import { Button } from "@/components/ui/button";
+import { useState } from "react";
+
+export default function Checkout({
+  searchParams,
+  planData,
+  clientToken,
+  user,
+  session,
+}) {
+  searchParams = JSON.parse(searchParams);
+  planData = JSON.parse(planData);
+  user = JSON.parse(user);
+  session = JSON.parse(session);
+
+  const [discount, setDiscount] = useState(null);
+  const [couponCode, setCouponCode] = useState(null);
+
+  return (
+    <section className="grid w-full grid-cols-1 gap-8 lg:grid-cols-7">
+      <section className="col-span-4">
+        <div className="">
+          <h2 className="flex items-center gap-4 pb-5 text-3xl">
+            <span>{session ? <Check /> : "1. "}</span>Sign Up
+          </h2>
+          {!session ? (
+            <section>
+              You are not logged in,{" "}
+              <Button variant="link">
+                <Link href={`/auth/login?callbackUrl=${header.get("referer")}`}>
+                  Login
+                </Link>{" "}
+              </Button>
+              first or SignUp.
+            </section>
+          ) : (
+            <div>
+              You are Logged in as
+              <Button variant="link">{session.user.email}</Button>
+            </div>
+          )}
+          <Separator className="my-4" />
+        </div>
+
+        <div>
+          <h2 className={`${!user && "text-secondary"} text-3xl`}>
+            2. Payment
+          </h2>
+          <div className={`${!user && "hidden"}`}>
+            <CheckOutForm
+              clientToken={clientToken}
+              user={user}
+              couponCode={couponCode}
+              planId={planData.id}
+            />
+          </div>
+        </div>
+      </section>
+      <section className="col-span-3">
+        <OrderSummary
+          plan={searchParams.plan}
+          price={planData.price}
+          duratation={planData.duratation}
+          discount={discount}
+          setDiscount={setDiscount}
+          setCouponCode={setCouponCode}
+        />
+      </section>
+    </section>
+  );
+}
