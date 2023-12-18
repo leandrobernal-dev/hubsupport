3a12f4a0e288232ecac038cd9bf16fe0935eac4e
leandrobernal-dev <108369489+leandrobernal-dev@users.noreply.github.com>
2023-12-18 08:39:09 +0800

    Add PaymentCard component for checkout

---
+++ src/app/product/checkout/components/PaymentCard.js
@@ -0,0 +1,190 @@
+import { Icons } from "@/components/icons";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import braintree from "braintree";
+
+async function makePayment() {
+  "use server";
+
+  const gateway = new braintree.BraintreeGateway({
+    environment: braintree.Environment.Sandbox,
+    merchantId: process.env.BRAINTREE_MERCHANT_ID,
+    publicKey: process.env.BRAINTREE_PUBLIC_KEY,
+    privateKey: process.env.BRAINTREE_PRIVATE_KEY,
+  });
+  // let clientToken = await gateway.clientToken.generate({}).then((response) => {
+  //   // pass clientToken to your front-end
+  //   return response.clientToken;
+  // });
+
+  const paymentMethod = await gateway.paymentMethod.create({});
+  const newCustomer = await gateway.customer
+    .create({
+      firstName: "Jen",
+      lastName: "Smith",
+      paymentMethodNonce: "fake-valid-nonce",
+    })
+    .then((result) => {
+      return result.customer.id;
+    });
+
+  gateway.subscription
+    .create({
+      planId: "business_monthly",
+    })
+    .then((result) => {
+      console.log(result);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
+
+export async function PaymentCard() {
+  return (
+    <form action={makePayment}>
+      <Card className="w-96">
+        <CardHeader>
+          <CardTitle>Payment Method</CardTitle>
+          <CardDescription>
+            Add a new payment method to your account.
+          </CardDescription>
+        </CardHeader>
+        <CardContent className="grid gap-6">
+          <RadioGroup defaultValue="paypal" className="grid grid-cols-3 gap-4">
+            <div>
+              <RadioGroupItem
+                disabled
+                value="card"
+                id="card"
+                className="peer sr-only"
+              />
+              <Label
+                htmlFor="card"
+                className="flex flex-col items-center justify-between rounded-md border-2 border-muted bg-popover p-4 hover:bg-accent hover:text-accent-foreground peer-data-[state=checked]:border-primary [&:has([data-state=checked])]:border-primary"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  className="mb-3 h-6 w-6"
+                >
+                  <rect width="20" height="14" x="2" y="5" rx="2" />
+                  <path d="M2 10h20" />
+                </svg>
+                Card
+              </Label>
+            </div>
+            <div>
+              <RadioGroupItem
+                value="paypal"
+                id="paypal"
+                className="peer sr-only"
+              />
+              <Label
+                htmlFor="paypal"
+                className="flex flex-col items-center justify-between rounded-md border-2 border-muted bg-popover p-4 hover:bg-accent hover:text-accent-foreground peer-data-[state=checked]:border-primary [&:has([data-state=checked])]:border-primary"
+              >
+                <Icons.paypal className="mb-3 h-6 w-6" />
+                Paypal
+              </Label>
+            </div>
+            <div>
+              <RadioGroupItem
+                disabled
+                value="apple"
+                id="apple"
+                className="peer sr-only"
+              />
+              <Label
+                htmlFor="apple"
+                className="flex flex-col items-center justify-between rounded-md border-2 border-muted bg-popover p-4 hover:bg-accent hover:text-accent-foreground peer-data-[state=checked]:border-primary [&:has([data-state=checked])]:border-primary"
+              >
+                <Icons.apple className="mb-3 h-6 w-6" />
+                Apple
+              </Label>
+            </div>
+          </RadioGroup>
+          <div className="grid gap-2">
+            <Label htmlFor="name">Name</Label>
+            <Input id="name" placeholder="First Last" />
+          </div>
+          <div className="grid gap-2">
+            <Label htmlFor="number">Card number</Label>
+            <Input id="number" placeholder="" />
+          </div>
+          <div className="grid grid-cols-3 gap-4">
+            <div className="grid gap-2">
+              <Label htmlFor="month">Expires</Label>
+              <Select>
+                <SelectTrigger id="month">
+                  <SelectValue placeholder="Month" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value="1">January</SelectItem>
+                  <SelectItem value="2">February</SelectItem>
+                  <SelectItem value="3">March</SelectItem>
+                  <SelectItem value="4">April</SelectItem>
+                  <SelectItem value="5">May</SelectItem>
+                  <SelectItem value="6">June</SelectItem>
+                  <SelectItem value="7">July</SelectItem>
+                  <SelectItem value="8">August</SelectItem>
+                  <SelectItem value="9">September</SelectItem>
+                  <SelectItem value="10">October</SelectItem>
+                  <SelectItem value="11">November</SelectItem>
+                  <SelectItem value="12">December</SelectItem>
+                </SelectContent>
+              </Select>
+            </div>
+            <div className="grid gap-2">
+              <Label htmlFor="year">Year</Label>
+              <Select>
+                <SelectTrigger id="year">
+                  <SelectValue placeholder="Year" />
+                </SelectTrigger>
+                <SelectContent>
+                  {Array.from({ length: 10 }, (_, i) => (
+                    <SelectItem
+                      key={i}
+                      value={`${new Date().getFullYear() + i}`}
+                    >
+                      {new Date().getFullYear() + i}
+                    </SelectItem>
+                  ))}
+                </SelectContent>
+              </Select>
+            </div>
+            <div className="grid gap-2">
+              <Label htmlFor="cvc">CVC</Label>
+              <Input id="cvc" placeholder="CVC" />
+            </div>
+          </div>
+        </CardContent>
+        <CardFooter>
+          <Button className="w-full">Continue</Button>
+        </CardFooter>
+      </Card>
+    </form>
+  );
+}
